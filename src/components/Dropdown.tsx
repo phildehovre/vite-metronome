a031efeed615614890741ce4752f7923b1b40bc2
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-10-09 17:51:11 +0100

    vercel tweaks

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,38 +1,34 @@
-import React from 'react'
-import DropdownItem from './DropdownItem'
+import DropdownItem from "./DropdownItem";
 
+const Dropdown = (props: any) => {
+  const { dropdownOpen, soundEffect, setSoundEffect, setDropdownOpen } = props;
 
-const Dropdown = (props) => {
-
-  const { dropdownOpen, soundEffect, setSoundEffect, setDropdownOpen } = props
-
-  const sounds = ['sidestick', 'cowbell', 'woodblock']
+  const sounds = ["sidestick", "cowbell", "woodblock"];
 
   const renderDropdown = () => {
     if (props.open) {
-      const newSounds = sounds.filter(sound => { return sound !== soundEffect })
-      return (
-        newSounds.map(sound => {
-          return (
-            <DropdownItem
-              sound={sound}
-              key={sound}
-              setSoundEffect={setSoundEffect}
-              setDropdownOpen={setDropdownOpen}
-              dropdownOpen={dropdownOpen}
-            />
-          )
-        })
-      )
+      const newSounds = sounds.filter((sound) => {
+        return sound !== soundEffect;
+      });
+      return newSounds.map((sound) => {
+        return (
+          <DropdownItem
+            sound={sound}
+            key={sound}
+            setSoundEffect={setSoundEffect}
+            setDropdownOpen={setDropdownOpen}
+            dropdownOpen={dropdownOpen}
+          />
+        );
+      });
     }
-  }
-
+  };
 
   return (
     <div className={`metro-dropdown${dropdownOpen ? ` open` : ` closed`}`}>
       {renderDropdown()}
     </div>
-  )
-}
+  );
+};
 
-export default Dropdown
+export default Dropdown;
--- src/components/DropdownItem.tsx
+++ src/components/DropdownItem.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const DropdownItem = (props) => {
-
-  const { setDropdownOpen, setSoundEffect, dropdownOpen, sound } = props
-
-  const handleClick = (e) => {
-    setSoundEffect(sound)
-    setDropdownOpen(!dropdownOpen)
-  }
-
-  return (
-    <div className="metro-dropdown-item" onClick={handleClick}>{sound}</div>
-  )
+interface DropdownItemProps {
+  setDropdownOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setSoundEffect: React.Dispatch<React.SetStateAction<string>>;
+  dropdownOpen: boolean;
+  sound: string;
 }
 
-export default DropdownItem
+const DropdownItem = (props: DropdownItemProps) => {
+  const { setDropdownOpen, setSoundEffect, dropdownOpen, sound } = props;
+
+  const handleClick = () => {
+    setSoundEffect(sound);
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  return (
+    <div className="metro-dropdown-item" onClick={handleClick}>
+      {sound}
+    </div>
+  );
+};
+
+export default DropdownItem;
